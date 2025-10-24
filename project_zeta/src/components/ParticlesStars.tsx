f12af9f1a7b981b1b994f7f36d2d42ac65b2e503
Trevor Eisenbacher <teisenba@vols.utk.edu>
2025-10-23 22:23:14 -0400

    Fixed window error with the particles import

--- project_zeta/src/components/ParticlesStars.tsx
+++ project_zeta/src/components/ParticlesStars.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { useEffect } from 'react';
 import styles from './ParticlesStars.module.css';
-import 'particles.js';
 
 export default function ParticlesStars() {
   useEffect(() => {
-    // @ts-ignore
-    particlesJS.load('particles-js', '/assets/particles.json', () => {
-      console.log('Particles config loaded');
+    import('particles.js').then(() => {
+      // @ts-ignore
+      particlesJS.load('particles-js', '/assets/particles.json', () => {
+        console.log('Particles config loaded');
+      });
     });
   }, []);
 
